483983d5e94dc3cbf9aad219d68014515c026af1
Prachita Nayak <pnayak@measureone.com>
2021-03-24 20:33:02 +0530

    tmp

--- server.js
+++ server.js
@@ -1,20 +1,32 @@
 const express = require("express");
 const apiHandler = require("./api-handler/api-function");
 const api = express();
-api.use(express.json())
+api.use(express.json());
 api.use(express.urlencoded());
 
 api.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type , Authorization");
-  res.setHeader("Access-Control-Request-Headers", "Content-Type ,Authorization");
+  res.setHeader(
+    "Access-Control-Request-Headers",
+    "Content-Type ,Authorization"
+  );
   res.setHeader("Access-Control-Allow-Credentials", true);
   next();
 });
-api.get("/states",async (req, res) =>await apiHandler.getStates(req,res));
-api.get("/banks",async (req, res) =>await apiHandler.getBanks(req,res));
-api.get("/cities/:state_id",async (req, res) =>await apiHandler.getCities(req,res));
-api.post("/branch",async (req, res) =>await apiHandler.getResults(req,res));
-api.post("/search",async(req,res)=>await apiHandler.getKeywordResults(req,res))
-api.listen(process.env.PORT||3001, () => console.log("Server is running on 3001"));
+api.get("/states", async (req, res) => await apiHandler.getStates(req, res));
+api.get("/banks", async (req, res) => await apiHandler.getBanks(req, res));
+api.get(
+  "/cities/:state_id",
+  async (req, res) => await apiHandler.getCities(req, res)
+);
+api.post("/branch", async (req, res) => await apiHandler.getResults(req, res));
+api.post(
+  "/search",
+  async (req, res) => await apiHandler.getKeywordResults(req, res)
+);
+api.post("/tmp", async (req, res) => await apiHandler.getTmp(req, res));
+api.listen(process.env.PORT || 3001, () =>
+  console.log("Server is running on 3001")
+);
