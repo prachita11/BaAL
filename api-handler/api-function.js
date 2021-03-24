f503e5dc46dbd05c03f9114680454f4de30cc06d
Prachita Nayak <pnayak@measureone.com>
2021-03-24 20:37:45 +0530

    tmp

--- api-handler/api-function.js
+++ api-handler/api-function.js
@@ -7,16 +7,18 @@ const Branch = require("../schema/Branch");
 connect();
 
 const getStates = async (req, res) => {
-  let states = await State.find({ country_id: 1 }).sort( { "state_id": 1 } );
+  let states = await State.find({ country_id: 1 }).sort({ state_id: 1 });
   return res.json({ success: true, data: states });
 };
 const getCities = async (req, res) => {
-  let cities = await City.find({ state_id: req.params.state_id }).sort( { "city_id": 1 } );
+  let cities = await City.find({ state_id: req.params.state_id }).sort({
+    city_id: 1,
+  });
   return res.json({ success: true, data: cities });
 };
 
 const getBanks = async (req, res) => {
-  let banks = await Bank.find({}).sort( { "bank_id": 1 } );
+  let banks = await Bank.find({}).sort({ bank_id: 1 });
   return res.json({ success: true, data: banks });
 };
 
@@ -25,40 +27,42 @@ const getResults = async (req, res) => {
   let city_id = req.body.city_id;
   let bank_id = req.body.bank_id;
   let type = req.body.type;
- let query={};
- if(state_id!==null){
- query.state_id=state_id;
- }
- if(city_id!==null)
- {
-   query.city_id=city_id;
- }
- if(bank_id!==null)
- {
-   query.bank_id = bank_id
- }
- if(type==2)
- {
-   query.is_bank=true;
- }else if(type==3)
- {
-   query.is_bank=false;
- }
- let results = await Branch.find(query,{_id:0});
- return sendResponse(results, res);
-
-  
+  let query = {};
+  if (state_id !== null) {
+    query.state_id = state_id;
+  }
+  if (city_id !== null) {
+    query.city_id = city_id;
+  }
+  if (bank_id !== null) {
+    query.bank_id = bank_id;
+  }
+  if (type == 2) {
+    query.is_bank = true;
+  } else if (type == 3) {
+    query.is_bank = false;
+  }
+  let results = await Branch.find(query, { _id: 0 });
+  return sendResponse(results, res);
 };
 
 const getKeywordResults = async (req, res) => {
- let arr = req.body.arr;
- var regex = [];
- let data;
- for (var i = 0; i < arr.length; i++) {
-     regex[i] = "."+arr[i]+".";
-      data = await Branch.find({"branch_address": {$regex: regex[i],$options:'i'}},{_id:0});
- } 
- return sendResponse(data, res);  
+  let arr = req.body.arr;
+  var regex = [];
+  let data;
+  for (var i = 0; i < arr.length; i++) {
+    regex[i] = "." + arr[i] + ".";
+    data = await Branch.find(
+      { branch_address: { $regex: regex[i], $options: "i" } },
+      { _id: 0 }
+    );
+  }
+  return sendResponse(data, res);
+};
+
+const getTmp = async (req, res) => {
+  console.log(JSON.parse(req.body));
+  return null;
 };
 
 const sendResponse = (results, res) => {
@@ -67,10 +71,12 @@ const sendResponse = (results, res) => {
   }
   return res.send({ status: true, data: results });
 };
+
 module.exports = {
   getStates,
   getCities,
   getBanks,
   getResults,
-  getKeywordResults
+  getKeywordResults,
+  getTmp,
 };
