2fabc17db3acb81fcd66677356c913fbef84f972
Prachita Nayak <pnayak@measureone.com>
2021-04-30 00:28:32 +0530

    file path changed

--- api-handler/api-function.js
+++ api-handler/api-function.js
@@ -282,7 +282,7 @@ const generateAPI = async (req, res, valid_email) => {
 
 const getAPIKit = async (req, res) => {
   res.type("Blob");
-  return res.sendFile("./BaAL.zip");
+  return res.sendFile("apiHandler/BaAL.zip");
 };
 
 const decrementLimit = async (req, res) => {
